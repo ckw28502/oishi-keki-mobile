6ac908927c73b14771ad8d5023c724688d55f5b7
ckw28502 <ckw28502@gmail.com>
2025-08-13 15:29:51 +0200

    Add FAB for adding new cake

--- oishi-keki-mobile/app/(owner)/index.tsx
+++ oishi-keki-mobile/app/(owner)/index.tsx
@@ -1,12 +1,18 @@
 import colors from "@/theme/colors";
 import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
+import { FAB } from "react-native-paper";
 
 const CakeScreen = () => {
     return(
-        <View style={styles.container}>
-            <Text>hi</Text>
-        </View>
+        <>
+            <View style={styles.container}>
+            </View>
+            <FAB
+                icon="plus"
+                size="medium"
+                style={styles.addFAB} 
+            />
+        </>
     );
 }
 
@@ -15,7 +21,11 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.backgroundColor
     },
-    
+    addFAB: {
+        position: "absolute",
+        right: "8%",
+        bottom: "5%"
+    },
 });
 
 export default CakeScreen;
